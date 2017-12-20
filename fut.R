71c79116f9d8a25c1b408905fea2fa65ea2a5b9b
Jheng-Yi Chen <deanst28@gmail.com>
2017-12-20 23:16:07 +0800

    Add files via upload

---
+++ fut.R
@@ -0,0 +1,17 @@
+
+setwd("C:/Users/CJY/Desktop/2016_fut")
+getwd()
+
+library(quantmod)
+library(zoo)
+
+futures2016 <- read.csv("2016_fut.csv", header = TRUE)
+keep <- seq(2, nrow(futures2016), by = 5)
+futures2016 <- futures2016[keep, ]
+futures2016 <- futures2016[, c(1, 4, 5, 6, 7, 10)]
+colnames(futures2016) <- c("Date", "Open", "High", "Low", "Close", "Volume")
+head(futures2016, 10)
+
+futures2016$Date <- gsub("/", "-", futures2016$Date)
+futures2016$Date <- as.Date(futures2016$Date, format = "%Y/%m/%d/")
+
