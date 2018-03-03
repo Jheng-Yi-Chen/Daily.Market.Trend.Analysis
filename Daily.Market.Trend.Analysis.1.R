23e6848e76bd14318a3e6ce7975ccf5dcf14eeb4
Jheng-Yi Chen <deanst28@gmail.com>
2018-03-03 23:00:15 +0800

    Update Daily.Market.Trend.Analysis.1.R

--- Daily.Market.Trend.Analysis.1.R
+++ Daily.Market.Trend.Analysis.1.R
@@ -1,17 +1,12 @@
-
 setwd("C:")
 getwd()
-
-library(quantmod)
-library(zoo)
 
 Data <- read.csv("Data.csv", header = TRUE)
 keep <- seq(2, nrow(Data), by = 5)
 Data <- Data[keep, ]
 Data <- Data[, c(1, 4, 5, 6, 7, 10)]
 colnames(Data) <- c("Date", "Open", "High", "Low", "Close", "Volume")
-head(futures2016, 10)
+head(Data, 10)
 
 Data$Date <- gsub("/", "-", Data$Date)
 Data$Date <- as.Date(Data$Date, format = "%Y/%m/%d/")
-
