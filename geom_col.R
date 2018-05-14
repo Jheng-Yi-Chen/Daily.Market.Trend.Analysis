35844b5e49417c438862e299de40baaf359e9fe6
Jheng-Yi Chen <deanst28@gmail.com>
2018-05-14 14:21:52 +0800

    Update geom_col.R

--- geom_col.R
+++ geom_col.R
@@ -1,25 +1,18 @@
-#------------------------------
+##########上櫃鋼鐵工業##########
 
-sop <- tw_all_company %>% filter(tse_otc == "TSE" & industry_cha == "水泥工業") %>%
+sop <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
   group_by(year) %>% 
   summarise(sum_operating_profit = sum(operating_profit))
 
 ggplot(sop, aes(x = year, y = sum_operating_profit)) + 
   geom_col(colour = "black") +
-  ggtitle("上市水泥營業利益總和") + xlab("年度") + ylab("營業利益總和") +
-  # theme(plot.title = element_text(color = "darkred", size = 26, hjust = .2),
-  #       axis.title.x = element_text(size = 14),
-  #       axis.title.y = element_text(size = 14),
-  #       axis.text.x = element_text(size = 12, hjust = .2),
-  #       axis.text.y = element_text(size = 12)) +
+  ggtitle("上櫃鋼鐵工業營業利益總和") + xlab("年度") + ylab("營業利益總和") +
   scale_y_continuous(labels = comma) +
   theme_wsj() + scale_colour_wsj("colors6")
-  # theme_igray() + scale_colour_tableau()
-  # theme_economist()
 
 #------------------------------
 
-sop <- tw_all_company %>% filter(tse_otc == "TSE" & industry_cha == "水泥工業") %>%
+sop <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
   group_by(year) %>% 
   summarise(sum_operating_profit = sum(operating_profit))
 
@@ -28,6 +21,59 @@ sop <- sop[-1, ]
 
 ggplot(sop, aes(x = year, y = margin_sum_operating_profit)) + 
   geom_col(colour = "black") +
-  ggtitle("上市水泥營業利益成長率") + xlab("年度") + ylab("營業利益成長率") +
+  ggtitle("上櫃鋼鐵工業營業利益成長率") + xlab("年度") + ylab("營業利益成長率") +
   theme_wsj() + scale_colour_wsj("colors6")
-  # theme_economist()
+
+#------------------------------
+
+sni <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
+  group_by(year) %>% 
+  summarise(sum_net_income = sum(net_income))
+
+ggplot(sni, aes(x = year, y = sum_net_income)) + 
+  geom_col(colour = "black") +
+  ggtitle("上櫃鋼鐵工業稅後淨利總和") + xlab("年度") + ylab("稅後淨利總和") +
+  scale_y_continuous(labels = comma) +
+  theme_wsj() + scale_colour_wsj("colors6")
+
+#------------------------------
+
+sni <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
+  group_by(year) %>% 
+  summarise(sum_net_income = sum(net_income))
+
+sni$margin_sum_net_income[2:7] <- diff(sni$sum_net_income)/sni$sum_net_income[1:6]
+sni <- sni[-1, ]
+
+ggplot(sni, aes(x = year, y = margin_sum_net_income)) + 
+  geom_col(colour = "black") +
+  ggtitle("上櫃鋼鐵工業稅後淨利成長率") + xlab("年度") + ylab("稅後淨利成長率") +
+  theme_wsj() + scale_colour_wsj("colors6")
+
+#------------------------------
+
+sr <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
+  group_by(year) %>% 
+  summarise(sum_revenue = sum(revenue))
+
+ggplot(sr, aes(x = year, y = sum_revenue)) + 
+  geom_col(colour = "black") +
+  ggtitle("上櫃鋼鐵工業營收總和") + xlab("年度") + ylab("營收總和") +
+  scale_y_continuous(labels = comma) +
+  theme_wsj() + scale_colour_wsj("colors6")
+
+#------------------------------
+
+sr <- tw_all_company %>% filter(tse_otc == "OTC" & industry_cha == "鋼鐵工業") %>%
+  group_by(year) %>% 
+  summarise(sum_revenue = sum(revenue))
+
+sr$margin_sum_revenue[2:7] <- diff(sr$sum_revenue)/sr$sum_revenue[1:6]
+sr <- sr[-1, ]
+
+ggplot(sr, aes(x = year, y = margin_sum_revenue)) + 
+  geom_col(colour = "black") +
+  ggtitle("上櫃鋼鐵工業營收成長率") + xlab("年度") + ylab("營收成長率") +
+  theme_wsj() + scale_colour_wsj("colors6")
+
+#------------------------------
