f5c66f7d33f9705f7c96b287d6b3f7406e6ff744
Jheng-Yi Chen <deanst28@gmail.com>
2018-05-24 10:37:25 +0800

    Add files via upload

---
+++ twmap.R
@@ -0,0 +1,14 @@
+install.packages("ggmap")
+library(ggmap)
+
+us <- c(left = -125, bottom = 25.75, right = -67, top = 49)
+us.map <- get_stamenmap(us, zoom = 5, maptype = "toner-lite")
+ggmap(us.map)
+
+
+tw <- c(left = 117.5, bottom = 20, right = 125, top = 27.5)
+tw.map <- get_stamenmap(tw, zoom = 5, maptype = "toner-lite")
+ggmap(tw.map)
+ggmap(tw.map, extent = "device")
+
+get_googlemap("taiwan", zoom = 12) %>% ggmap()
