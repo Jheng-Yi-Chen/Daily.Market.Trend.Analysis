dc0510ce986bacbff78184d0bcbcf4d76107d7c9
Jheng-Yi Chen <deanst28@gmail.com>
2020-05-28 15:23:59 +0800

    Add files via upload

--- TFE-2020-05-27.R
+++ TFE-2020-05-27.R
@@ -17,27 +17,29 @@ TX <- TX[c(-7, -8, -9)]
 
 is.na(TX) %>% sum()
 
-
 #TX1 <- TX %>% filter(Symbol == "TX")
 #View(TX1)
 
-timeCharVector = paste(TX$Date, TX$Time)
-timeVector = strptime(timeCharVector, "%Y%m%d %H%M%S", tz = Sys.timezone())
-
-TX2 <- TX %>% na.omit() 
-timeVector <- timeVector %>% na.omit()
-
-duration <- "2020-05-26 08:45:00::2020-05-26 13:45:00"
-
 TX2 <- TX %>% filter(Date == 20200526)
-
 
 TX2 <- TX %>% filter(as.numeric(TX$Time) >= 84500)
 TX2 <- TX %>% filter(as.numeric(TX$Time) <= 134500)
 TX2 <- TX %>% filter(as.numeric(TX$Time) >= 84500 & as.numeric(TX$Time) <= 134500)
 
+timeCharVector = paste(TX$Date, TX$Time)
+timeVector = strptime(timeCharVector, "%Y%m%d %H%M%S", tz = Sys.timezone())
 
-TX3 <- xts(TX2[ ,c(5:6)], as.POSIXct(timeVector, duration, tz = Sys.timezone()))
+DateTime <- ymd_hms(paste(TX$Date, TX$Time), tz = Sys.timezone())
+test <- paste(DateTime, TX[c(5,6)])
+
+TX3 <- TX2 %>% na.omit() 
+timeVector <- timeVector %>% na.omit()
+
+duration <- "2020-05-26 08:45:00::2020-05-26 13:45:00"
+
+TX3 <- cbind(TX2[ ,c(5:6)], as.POSIXct(timeVector, duration, tz = Sys.timezone()))
+
+TX4 <- xts(TX3[ ,c(5:6)], as.POSIXct(timeVector, duration, tz = Sys.timezone()))
 #TX3 <- TX %>% filter(Delivery == 202006)
 #TX3 <- TX[TX$Delivery == "202006", ]
 
